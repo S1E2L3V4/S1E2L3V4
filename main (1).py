903fd0e4e95c182c2443d4db9723252d7172dfe5
S1E2L3V4 <145092539+S1E2L3V4@users.noreply.github.com>
2023-09-15 20:26:19 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+def isLeapyear(year):
+  if (year÷4==o and year÷ 100 !=0)
+  return true 
+  else :
+  return False
+  year= 2023
+  if is leapyear(year):
+    print('({} is a leap year.')
+  
